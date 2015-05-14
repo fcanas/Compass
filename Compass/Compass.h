04ea7a4e93d9f49635bb73f3c6b0350ae8506a1e
Fabian Canas <fcanas@gmail.com>
2015-05-14 16:26:47 -0400

    Initial commit

---
+++ Compass/Compass.h
@@ -0,0 +1,19 @@
+//
+//  Compass.h
+//  Compass
+//
+//  Created by Fabian Canas on 5/14/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Compass.
+FOUNDATION_EXPORT double CompassVersionNumber;
+
+//! Project version string for Compass.
+FOUNDATION_EXPORT const unsigned char CompassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Compass/PublicHeader.h>
+
+

5ce503ed540b3feda0d211661054aa38ba0cd88b
Fabian Canas <fcanas@gmail.com>
2015-06-03 17:17:55 -0400

    Fix geo-vector function and add bearing function

--- Compass/GeoVector.swift
+++ Compass/GeoVector.swift
@@ -8,6 +8,12 @@
 
 import CoreLocation
 
+func bearing(start: CLLocationCoordinate2D, end: CLLocationCoordinate2D)  -> Radian {
+    let (lat1, lng1) = start.asRadians
+    let (lat2, lng2) = end.asRadians
+    return Radian(atan2(sin(lng1 - lng2) * cos(lat2), cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(lng1-lng2)))
+}
+
 extension Vector3 {
     public init(coordinate: CLLocationCoordinate2D) {
         let latitude :Radian = Radian(angle: Degree(coordinate.latitude))
@@ -20,8 +26,15 @@ extension Vector3 {
 
 extension CLLocationCoordinate2D {
     init(v: Vector3) {
-        longitude = atan( v.dy / v.dx )
-        latitude = acos( v.dz / magnitude(v) )
+        longitude = Degree(angle:Radian(atan2( v.dy, v.dx ))).value
+        latitude = Degree(angle: Radian(acos( v.dz / magnitude(v) ))).value
+        assert(CLLocationCoordinate2DIsValid(self), "Making an invalid coordinate from a vector")
+    }
+    
+    var asRadians :(latitude: Radian, longitude: Radian){
+        get {
+            return (latitude: Radian(angle:Degree(self.latitude)), longitude: Radian(angle:Degree(self.latitude)))
+        }
     }
 }
 
